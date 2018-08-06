d799e77bd0c4dbddf918dc66877dce4b6af939e7
bbiswal333 <bbiswal333@gmail.com>
2018-08-06 21:02:54 +0530

    Update CloudDataSourceConfig.java

--- src/main/java/com/sap/refapps/espm/config/CloudDataSourceConfig.java
+++ src/main/java/com/sap/refapps/espm/config/CloudDataSourceConfig.java
@@ -20,7 +20,7 @@ import org.springframework.context.annotation.Profile;
  *
  */
 @Configuration
-@Profile("cloud")
+@Profile("local")
 public class CloudDataSourceConfig /*extends AbstractCloudConfig*/  {
 
 	/**
