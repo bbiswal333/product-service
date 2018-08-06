6cbd0258ab7ffd5cfb5ae0c48caab3d073e3f9b9
bbiswal333 <bbiswal333@gmail.com>
2018-08-06 20:48:03 +0530

    Update CloudDataSourceConfig.java

--- src/main/java/com/sap/refapps/espm/config/CloudDataSourceConfig.java
+++ src/main/java/com/sap/refapps/espm/config/CloudDataSourceConfig.java
@@ -33,20 +33,40 @@ public class CloudDataSourceConfig /*extends AbstractCloudConfig*/  {
 	public DataSource cloudDataSource() {
 		return connectionFactory().dataSource();
 	}*/
-	 @Bean
-    public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+	
+	
+	Logger log = LoggerFactory.getLogger(getClass());
+
+    @Bean
+    public DataSource postgresDataSource() {
+        String databaseUrl = System.getenv("DATABASE_URL")
+        log.info("Initializing PostgreSQL database: {}", databaseUrl);
+
+        URI dbUri;
+        try {
+            dbUri = new URI(databaseUrl);
+        }
+        catch (URISyntaxException e) {
+            log.error(String.format("Invalid DATABASE_URL: %s", databaseUrl), e);
+            return null;
+        }
 
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' 
+                       + dbUri.getPort() + dbUri.getPath();
 
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
-
-        return basicDataSource;
+        org.apache.tomcat.jdbc.pool.DataSource dataSource 
+            = new org.apache.tomcat.jdbc.pool.DataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setTestOnBorrow(true);
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnReturn(true);
+        dataSource.setValidationQuery("SELECT 1");
+        return dataSource;
     }
 
 }
