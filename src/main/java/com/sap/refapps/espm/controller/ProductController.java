fe9c68c8e2c6941c186be0f0ba68670f1adb18d4
c5244525 <bismay.kumar.biswal@sap.com>
2018-08-07 18:52:57 +0530

    cross origin configured

--- src/main/java/com/sap/refapps/espm/controller/ProductController.java
+++ src/main/java/com/sap/refapps/espm/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -38,6 +39,7 @@ public class ProductController {
 	 * @return list of product
 	 * @throws InterruptedException 
 	 */
+	@CrossOrigin(origins = "https://product-ui.herokuapp.com/")
 	@GetMapping("/products")
 	public ResponseEntity<Iterable<Product>> getAllProducts() throws InterruptedException {
 		final Iterable<Product> products;
